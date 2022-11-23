f3d3e8ba470add639e2bf9416d4eb64461d80dc9
AndreyPiganov <andrey.piganov@mail.ru>
2022-11-23 10:23:50 +0300

    update files

--- bin/magic_ball.js
+++ bin/magic_ball.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
+import { Magic,UserQuestion } from '../src/ball.js';
 
-import Magic from '../src/ball.js';
-
+console.log("Greetings, traveler-it's me, the magic ball.");
+console.log(UserQuestion());
 console.log(Magic());
